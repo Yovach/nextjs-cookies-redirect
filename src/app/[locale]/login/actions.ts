20665b497785fedad622132ea39ca6c5b8ac42cb
Quentin <contact@yovach.dev>
2023-10-19 15:42:06 +0200

    ref: don't throw error if no username

--- src/app/[locale]/login/actions.ts
+++ src/app/[locale]/login/actions.ts
@@ -4,8 +4,9 @@ import { cookies } from "next/headers";
 import { RedirectType, redirect } from "next/navigation";
 export async function login(data: FormData) {
   const username = data.get("username");
-  if (!username) throw new Error("Missing username");
 
-  cookies().set("username", username.toString());
+  if (username) {
+    cookies().set("username", username.toString());
+  }
   redirect("/profile", RedirectType.push);
 }
